9041263711656499d1bd376869fc181934b8d382
Bay-gong <baeaqi21@gmail.com>
2023-11-17 19:40:48 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Coba Git Remote</title>
+</head>
+<body>
+
+	<h1>Hello World!</h1>
+	<p>Lorem, ipsum dolor sit, amet consectetur adipisicing elit. Nostrum iusto quibusdam aperiam fuga eaque, corrupti sapiente necessitatibus doloribus ut inventore saepe ex nesciunt alias hic? Blanditiis, provident quibusdam at eligendi!</p>
+	
+</body>
+</html>
